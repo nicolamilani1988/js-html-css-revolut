e0d9dc4f6728d1274224bfad2e720b70dbf70bea
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-03-23 15:59:40 +0100

    create funzioni globali per tutti i dropdown.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function init(){
+function showMenu(){
+  console.log("hoooover");
+  $(".dropdown-menu").removeClass("hide");
+}
 
+function removeMenu (){
+
+    $(".dropdown-menu").addClass("hide");
+
+
+}
+
+function init(){
+  $(".dropdown-start").hover(showMenu);
+  $("header").click(removeMenu);
 }
 
 $(document).ready(init);
